9998980ad36a6651dff5929bad86be221ef5c619
DuStein <steindu@gmail.com>
2022-04-17 11:42:32 -0300

    criando cadastrar user

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,17 @@
 const mongooose = require('mongoose');
 
 
-const userSchema = new mongoose.Schema();
+const userSchema = new mongoose.Schema({
+    id: 'string',
+    apelido: 'string',
+    nome: 'string',
+    funcional: 'string', 
+    telefone: 'string',
+    email: 'string',
+    unidade: 'string',
+    plantao: 'string'
+});
+
+const User = mongooose.model('User', userSchema);
+
 
--- src/server.js
+++ src/server.js
@@ -3,16 +3,39 @@ require('dotenv').config();
 
 const URL = process.env.URI;
 
-
-
-
 const app = express();
 app.use(express.json());
+
+const users = [];
+
+function makeNewUser(id, apelido, nome, funcional, telefone, email, unidade, plantao) {
+    const newUser = {
+        id: id,
+        apelido: apelido,
+        nome: nome,
+        funcional: funcional,
+        telefone: telefone,
+        email: email,
+        unidade: unidade,
+        plantao: plantao
+        };
+    users.push(newUser);
+    return newUser;
+};
 
 app.get("/", (req, res) => {
     res.json({message: "Hello World!Let`s GO"});
 });
 
+app.get('/user', (req, res) => {
+    res.json(users);
+});
 
+app.post('/user', (req, res) => {
+    const { id, apelido, nome, funcional, telefone, email, unidade, plantao } = req.body;
+    newUser = makeNewUser(id, apelido, nome, funcional, telefone, email, unidade, plantao);
+    console.log(newUser)
+    res.status(200).send(newUser);
+});
 
 app.listen(3000, () => console.log('Servidor ATIVO!'));
