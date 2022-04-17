85bda730e32810bff7c3df8ff1e88de495e833a1
DuStein <steindu@gmail.com>
2022-04-17 13:13:17 -0300

    iniciando criacao de propostas

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 const users = [];
+const propose = [];
 
 function makeNewUser(id, apelido, nome, funcional, telefone, email, unidade, plantao) {
     const newUser = {
@@ -18,24 +19,52 @@ function makeNewUser(id, apelido, nome, funcional, telefone, email, unidade, pla
         email: email,
         unidade: unidade,
         plantao: plantao
-        };
+    };
     users.push(newUser);
     return newUser;
 };
 
-app.get("/", (req, res) => {
-    res.json({message: "Hello World!Let`s GO"});
-});
+function makePropose(proposeId, userIdPropose) {
+    const newPropose = {
+        proposeId: id,
+        userIdPropose: userIdPropose,
+        userIdAccepted: 'Não Confirmado',
+        status: false
+    }
+    propose.push(newPropose);
+    return newPropose;
+};
+
+function acceptPropose(proposeId) {
+    const selectedPropose = propose.map(id => id === proposeId);
+    return selectedPropose;
+}
 
 app.get('/user', (req, res) => {
-    res.json(users);
+    res.status(200).json(users);
 });
+
+app.get('/propose', (req, res) => {
+    res.status(200).json(propose);
+});
+
 
 app.post('/user', (req, res) => {
     const { id, apelido, nome, funcional, telefone, email, unidade, plantao } = req.body;
     newUser = makeNewUser(id, apelido, nome, funcional, telefone, email, unidade, plantao);
     console.log(newUser)
-    res.status(200).send(newUser);
+    res.status(201).send(newUser);
 });
+
+app.post('/propose', (req, res) => {
+    const { proposeId, userIdPropose } = req.body;
+    newPropose = makePropose(proposeId, userIdPropose);
+    res.status(201).json(newPropose);
+});
+
+app.put('/propose', (req, res) => {
+    const selectedPropose = acceptPropose(proposeId);
+    
+})
 
 app.listen(3000, () => console.log('Servidor ATIVO!'));
